fb0854807f4ae09b6ef5dcc6b6a88b7ebf72b5e5
GHYounesse <ghalloumyounes2003@gmail.com>
2026-09-14 16:01:52 +0100

    Merge login/register into AuthController
    Split the leftover user list/show/delete actions into their own
    UserController rather than leaving them in a controller named Login.

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -10,9 +10,31 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class RegistrationController extends AbstractController
+class AuthController extends AbstractController
 {
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(): void
+    {
+        // controller can be blank: it will never be called! The logout
+        // is intercepted by the "logout" key on the firewall in security.yaml.
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
@@ -21,17 +43,12 @@ class RegistrationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
             $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
+                $userPasswordHasher->hashPassword($user, $form->get('plainPassword')->getData())
             );
 
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email
 
             return $this->redirectToRoute('app_login');
         }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,61 +9,26 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
-  class LoginController extends AbstractController
-  {
-      #[Route('/login', name: 'app_login')]
-
-     public function index(AuthenticationUtils $authenticationUtils): Response
-      {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app_home');
-        }
-         // get the login error if there is one
-         $error = $authenticationUtils->getLastAuthenticationError();
-
-         // last username entered by the user
-         $lastUsername = $authenticationUtils->getLastUsername();
-
-          return $this->render('login/index.html.twig', [
-
-             'last_username' => $lastUsername,
-             'error'         => $error,
-          ]);
-      }
-
-    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
-    public function logout()
-    {
-        // controller can be blank: it will never be called!
-        throw new \Exception('Don\'t forget to activate logout in security.yaml');
-    }
-
-    private $passwordEncoder;
-
-    public function __construct(UserPasswordHasherInterface $passwordEncoder)
-    {
-        $this->passwordEncoder= $passwordEncoder;
-    }
-
+class UserController extends AbstractController
+{
     #[Route(path: '/list', name: 'list')]
-    public function list(EntityManagerInterface $entityManager){
+    public function list(EntityManagerInterface $entityManager): Response
+    {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
         $users = $entityManager->getRepository(User::class)->findAll();
-        return $this->render('user/index.html.twig',['users'=>$users]);
 
+        return $this->render('user/index.html.twig', ['users' => $users]);
     }
-    #[Route(path: '/user/{id}', name: 'user_show')]
-    public function show(int $id,EntityManagerInterface $entityManager){
 
+    #[Route(path: '/user/{id}', name: 'user_show')]
+    public function show(int $id, EntityManagerInterface $entityManager): Response
+    {
         $user = $entityManager->getRepository(User::class)->find($id);
         if (!$user) {
             throw new NotFoundHttpException('User not found.');
@@ -72,8 +37,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
             throw new AccessDeniedException('You can only view your own profile.');
         }
 
-        return $this->render('user/show.html.twig',['user'=>$user]);
-
+        return $this->render('user/show.html.twig', ['user' => $user]);
     }
 
     #[Route(path: '/user/delete/{id}', name: 'user_delete', methods: ['POST'])]
@@ -91,7 +55,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
         }
         $entityManager->remove($user);
         $entityManager->flush();
-        return $this->redirectToRoute('app_home');
 
+        return $this->redirectToRoute('app_home');
     }
-  }
+}
